5f187c24061a255d98e247d770e1f0c9ad8564d8
Nimshi Silva <nimshisilva07@gmail.com>
2025-10-02 13:37:48 +0530

    added role based autherization

--- src/routes/route.routes.js
+++ src/routes/route.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { getRoutes, addRoute } = require("../controllers/routes.controller");
 const auth = require("../middleware/auth.middleware");
+const roleMiddleware = require("../middleware/role.middleware");
 
 const router = express.Router();
-router.get("/", getRoutes);
-router.post("/", auth, addRoute);
+router.get("/", getRoutes); // Public - anyone can view routes
+router.post("/", auth, roleMiddleware(["admin"]), addRoute); // Only admin can add new routes
 
 module.exports = router;
