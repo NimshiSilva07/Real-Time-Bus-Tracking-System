5289fa7cefb17c69b4145a07b42594ba937cf869
Nimshi Silva <nimshisilva07@gmail.com>
2025-10-02 12:24:26 +0530

    added trip controller

--- src/controllers/trips.controller.js
+++ src/controllers/trips.controller.js
@@ -1,47 +1,15 @@
-import Trip from '../models/trip.model.js';
-import Bus from '../models/bus.model.js';
+const Trip = require("../models/trip.model");
 
-export const createTrip = async (req, res) => {
-  try {
-    const t = await Trip.create(req.body);
-    res.status(201).json(t);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+exports.getTrips = async (req, res) => {
+  const trips = await Trip.find().populate("bus route");
+  res.json(trips);
 };
 
-export const listTrips = async (req, res) => {
+exports.addTrip = async (req, res) => {
   try {
-    const q = {};
-    if (req.query.route) q.route = req.query.route;
-    if (req.query.status) q.status = req.query.status;
-    const trips = await Trip.find(q).populate('bus').populate('route').sort({ departureTime: 1 });
-    res.json(trips);
+    const trip = await Trip.create(req.body);
+    res.status(201).json(trip);
   } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-export const getTrip = async (req, res) => {
-  try {
-    const t = await Trip.findById(req.params.id).populate('bus').populate('route');
-    if (!t) return res.status(404).json({ message: 'Not found' });
-    res.json(t);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// mark trip ongoing/completed
-export const updateTripStatus = async (req, res) => {
-  try {
-    const t = await Trip.findById(req.params.id);
-    if (!t) return res.status(404).json({ message: 'Not found' });
-    t.status = req.body.status || t.status;
-    if (req.body.arrivalTime) t.arrivalTime = req.body.arrivalTime;
-    await t.save();
-    res.json(t);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(400).json({ message: err.message });
   }
 };
